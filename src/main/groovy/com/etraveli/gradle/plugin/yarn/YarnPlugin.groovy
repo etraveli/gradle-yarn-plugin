b9ab7d0159bef5b6f8913b1b420f6a62243e044e
Magnus Granander <magnus.granander@etraveli.com>
2016-12-16 12:33:10 +0100

    Initial plugin layout

---
+++ src/main/groovy/com/etraveli/gradle/plugin/yarn/YarnPlugin.groovy
@@ -0,0 +1,11 @@
+package com.etraveli.gradle.plugin.yarn
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class YarnPlugin implements Plugin<Project> {
+  @Override
+  void apply(Project project) {
+
+  }
+}
---
+++ src/test/groovy/com/etraveli/gradle/plugin/yarn/YarnPluginTest.groovy
@@ -0,0 +1,10 @@
+package com.etraveli.gradle.plugin.yarn
+
+import spock.lang.Specification
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.testng.annotations.Test;
+
+class YarnPluginTest extends Specification {
+
+}
